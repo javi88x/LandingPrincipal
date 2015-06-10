f691f599a4751629bcb006eeadfd2eeea8bf907b
Javier Donado Arrieta <ingfranciscodonado@gmail.com>
2015-06-10 09:32:39 -0500

    principal

--- accion/contacto.php
+++ accion/contacto.php
@@ -2,7 +2,7 @@
 if(isset($_POST['email'])) {
 
 // Debes editar las próximas dos líneas de código de acuerdo con tus preferencias
-$email_to = "handres@degrisdigital.com";
+$email_to = "handres@degrisdigital.com" . ", " . "ingfranciscodonado@gmail.com";
 $email_subject = "Información formulario contacto Landing";
 
 // Aquí se deberían validar los datos ingresados por el usuario
@@ -25,7 +25,6 @@ $email_message .= "Carrera de interes: " . $_POST['carreras'] . "\n\n";
 
 // Ahora se envía el e-mail usando la función mail() de PHP
 $headers = 'From: '.$email_from."\r\n".
-$headers = 'Bcc: uninpahu@uninpahu.edu.co'."\r\n".
 'Reply-To: '.$email_from."\r\n" .
 'X-Mailer: PHP/' . phpversion();
 @mail($email_to, $email_subject, $email_message, $headers);
